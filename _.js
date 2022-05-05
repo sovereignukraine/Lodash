ba0db87b5786d45d18d9501a23b517b7a9913c6d
Volodymyr Makhobei <bratikua@gmail.com>
2022-05-05 22:37:14 +0300

    inRange() method added

--- _.js
+++ _.js
@@ -3,10 +3,21 @@ clamp: function(number, lower, upper){
    let lowerClampedValue = Math.max(number, lower);
    let clampedValue = Math.min(lowerClampedValue, upper);
    return clampedValue;
+},
+inRange: function(number, start, end){
+   if(end === undefined && start > 0){
+    return (number >= 0)&&(number < start);
+   }else if(end === undefined && start < 0){
+      return (number >= start)&&(number < 0);
+   }
+   if(start > end){
+      return (number >= end)&&(number < start);
+   }
+return (number >= start)&&(number < end); 
 }
 };
 
-
+console.log(_.inRange(5, 5, -5));
 
 // Do not write or modify code below this line.
 module.exports = _;
